21835a1a40897f8e81c036c3901be587b2ec0dcc
Yanxiao Zhao <zyxgle@gmail.com>
2014-03-07 09:02:59 +0800

    更新3.7.1

---
+++ smartITOM_IPhone/smartITOM_IPhone/EquipmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  EquipmentViewController.h
+//  smartITOM_IPhone
+//
+//  Created by Apple Developer on 14-3-5.
+//  Copyright (c) 2014年 Apple002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EquipmentViewController : UITableViewController
+
+@end
